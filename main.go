8cfd70fbd1d7b5dbbb85f3302a57292591f7e5e7
SergeyMoshkov <sergejmoshkov@gmail.com>
2022-12-13 23:54:08 +0300

    added two pages Home and About

--- main.go
+++ main.go
@@ -6,14 +6,33 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			log.Println("Warning...")
-		}
-		fmt.Println(n)
-	})
+// Home it is function for home page
+func Home(w http.ResponseWriter, _ *http.Request) {
+	sum := AddValues(2, 4)
+	n, err := fmt.Fprintf(w, "This is home page and sum 2 + 4 is %d", sum)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(n)
+}
 
-	http.ListenAndServe(":8080", nil)
+// About it is function for home page
+func About(w http.ResponseWriter, _ *http.Request) {
+	sum := AddValues(2, 2)
+	n, err := fmt.Fprintf(w, "This is about page and sum 2 + 2 is %d", sum)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(n)
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(":8080", nil)
 }
