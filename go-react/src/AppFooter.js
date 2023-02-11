b297f6d75d20781b990c18969b483ba671a307e8
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-02-11 16:27:48 +0300

    init commit

---
+++ go-react/src/AppFooter.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+
+class AppFooter extends Component {
+
+    render() {
+        const currentYear = new Date().getFullYear();
+        return (
+            <Fragment>
+                <hr />
+                <p>Copyright &copy; 2020 - { currentYear} Acme Ltd.</p>
+            </Fragment>
+        );
+    }
+}
+
+export default AppFooter
